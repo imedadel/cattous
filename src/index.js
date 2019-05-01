290b0a764f4f34df7bf44f2105ffb030c592dade
Imed Adel <aadelimed@gmail.com>
2019-05-01 22:45:20 +0100

    Add ThemeProvider and GlobalStyle

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import styled from "styled-components";
+import styled, { ThemeProvider, createGlobalStyle, css } from "styled-components";
 import {
   style,
   space,
@@ -752,11 +752,259 @@ const Div = styled("div")(
   themed("Div")
 );
 
-// export { cssProps };
-export const Flex = styled(Div)(
-  {
-    display: "flex"
-  },
-  themed("Flex")
-);
+const normalize = css`
+  html {
+    line-height: 1.15;
+    -webkit-text-size-adjust: 100%;
+  }
+  body {
+    margin: 0;
+  }
+  main {
+    display: block;
+  }
+  h1 {
+    font-size: 2em;
+    margin: 0.67em 0;
+  }
+  hr {
+    box-sizing: content-box;
+    height: 0;
+    overflow: visible;
+  }
+  pre {
+    font-family: monospace, monospace;
+    font-size: 1em;
+  }
+  a {
+    background-color: transparent;
+  }
+  abbr[title] {
+    border-bottom: none;
+    text-decoration: underline;
+    text-decoration: underline dotted;
+  }
+  b,
+  strong {
+    font-weight: bolder;
+  }
+  code,
+  kbd,
+  samp {
+    font-family: monospace, monospace;
+    font-size: 1em;
+  }
+  small {
+    font-size: 80%;
+  }
+  sub,
+  sup {
+    font-size: 75%;
+    line-height: 0;
+    position: relative;
+    vertical-align: baseline;
+  }
+  sub {
+    bottom: -0.25em;
+  }
+  sup {
+    top: -0.5em;
+  }
+  img {
+    border-style: none;
+  }
+  button,
+  input,
+  optgroup,
+  select,
+  textarea {
+    font-family: inherit;
+    font-size: 100%;
+    line-height: 1.15;
+    margin: 0;
+  }
+  button,
+  input {
+    overflow: visible;
+  }
+  button,
+  select {
+    text-transform: none;
+  }
+  button,
+  [type="button"],
+  [type="reset"],
+  [type="submit"] {
+    -webkit-appearance: button;
+  }
+  button::-moz-focus-inner,
+  [type="button"]::-moz-focus-inner,
+  [type="reset"]::-moz-focus-inner,
+  [type="submit"]::-moz-focus-inner {
+    border-style: none;
+    padding: 0;
+  }
+  button:-moz-focusring,
+  [type="button"]:-moz-focusring,
+  [type="reset"]:-moz-focusring,
+  [type="submit"]:-moz-focusring {
+    outline: 1px dotted ButtonText;
+  }
+  fieldset {
+    padding: 0.35em 0.75em 0.625em;
+  }
+  legend {
+    box-sizing: border-box;
+    color: inherit;
+    display: table;
+    max-width: 100%;
+    padding: 0;
+    white-space: normal;
+  }
+  progress {
+    vertical-align: baseline;
+  }
+  textarea {
+    overflow: auto;
+  }
+  [type="checkbox"],
+  [type="radio"] {
+    box-sizing: border-box;
+    padding: 0;
+  }
+  [type="number"]::-webkit-inner-spin-button,
+  [type="number"]::-webkit-outer-spin-button {
+    height: auto;
+  }
+  [type="search"] {
+    -webkit-appearance: textfield;
+    outline-offset: -2px;
+  }
+  [type="search"]::-webkit-search-decoration {
+    -webkit-appearance: none;
+  }
+  ::-webkit-file-upload-button {
+    -webkit-appearance: button;
+    font: inherit;
+  }
+  details {
+    display: block;
+  }
+  summary {
+    display: list-item;
+  }
+  template {
+    display: none;
+  }
+  [hidden] {
+    display: none;
+  }
+`;
+const normalizeOpenType = css`
+  ::-moz-selection {
+    color: inherit;
+    text-shadow: inherit;
+    background-color: #accef7;
+  }
+  ::selection {
+    color: inherit;
+    text-shadow: inherit;
+    background-color: #accef7;
+  }
+  html,
+  body,
+  table {
+    -webkit-font-feature-settings: "kern" 1, "liga" 1, "calt" 1, "pnum" 1,
+      "tnum" 0, "onum" 1, "lnum" 0, "dlig" 0;
+    -moz-font-feature-settings: "kern" 1, "liga" 1, "calt" 1, "pnum" 1, "tnum" 0,
+      "onum" 1, "lnum" 0, "dlig" 0;
+    font-feature-settings: "kern" 1, "liga" 1, "calt" 1, "pnum" 1, "tnum" 0,
+      "onum" 1, "lnum" 0, "dlig" 0;
+  }
+  h1,
+  h2,
+  h3 {
+    -webkit-font-feature-settings: "kern" 1, "liga" 1, "calt" 1, "pnum" 1,
+      "tnum" 0, "onum" 1, "lnum" 0, "dlig" 1;
+    -moz-font-feature-settings: "kern" 1, "liga" 1, "calt" 1, "pnum" 1, "tnum" 0,
+      "onum" 1, "lnum" 0, "dlig" 1;
+    font-feature-settings: "kern" 1, "liga" 1, "calt" 1, "pnum" 1, "tnum" 0,
+      "onum" 1, "lnum" 0, "dlig" 1;
+  }
+  abbr {
+    text-transform: uppercase;
+    -webkit-font-feature-settings: "kern" 1, "liga" 1, "calt" 1, "pnum" 1,
+      "tnum" 0, "onum" 1, "lnum" 0, "smcp" 1, "c2sc" 1;
+    -moz-font-feature-settings: "kern" 1, "liga" 1, "calt" 1, "pnum" 1, "tnum" 0,
+      "onum" 1, "lnum" 0, "smcp" 1, "c2sc" 1;
+    font-feature-settings: "kern" 1, "liga" 1, "calt" 1, "pnum" 1, "tnum" 0,
+      "onum" 1, "lnum" 0, "smcp" 1, "c2sc" 1;
+  }
+  time {
+    -webkit-font-feature-settings: "kern" 1, "liga" 1, "calt" 1, "pnum" 1,
+      "tnum" 0, "onum" 1, "lnum" 0;
+    -moz-font-feature-settings: "kern" 1, "liga" 1, "calt" 1, "pnum" 1, "tnum" 0,
+      "onum" 1, "lnum" 0;
+    font-feature-settings: "kern" 1, "liga" 1, "calt" 1, "pnum" 1, "tnum" 0,
+      "onum" 1, "lnum" 0;
+  }
+  pre,
+  kbd,
+  samp,
+  code {
+    -webkit-font-feature-settings: "kern" 0, "liga" 0, "calt" 1, "dlig" 0,
+      "pnum" 0, "tnum" 1, "onum" 0, "lnum" 1, "zero" 1;
+    -moz-font-feature-settings: "kern" 0, "liga" 0, "calt" 1, "dlig" 0, "pnum" 0,
+      "tnum" 1, "onum" 0, "lnum" 1, "zero" 1;
+    font-feature-settings: "kern" 0, "liga" 0, "calt" 1, "dlig" 0, "pnum" 0,
+      "tnum" 1, "onum" 0, "lnum" 1, "zero" 1;
+  }
+  sup {
+    -webkit-font-feature-settings: "kern" 1, "liga" 1, "calt" 1, "pnum" 1,
+      "tnum" 0, "onum" 1, "lnum" 0, "dlig" 0, "sups" 1;
+    -moz-font-feature-settings: "kern" 1, "liga" 1, "calt" 1, "pnum" 1, "tnum" 0,
+      "onum" 1, "lnum" 0, "dlig" 0, "sups" 1;
+    font-feature-settings: "kern" 1, "liga" 1, "calt" 1, "pnum" 1, "tnum" 0,
+      "onum" 1, "lnum" 0, "dlig" 0, "sups" 1;
+  }
+  sub {
+    -webkit-font-feature-settings: "kern" 1, "liga" 1, "calt" 1, "pnum" 1,
+      "tnum" 0, "onum" 1, "lnum" 0, "dlig" 0, "subs" 1;
+    -moz-font-feature-settings: "kern" 1, "liga" 1, "calt" 1, "pnum" 1, "tnum" 0,
+      "onum" 1, "lnum" 0, "dlig" 0, "subs" 1;
+    font-feature-settings: "kern" 1, "liga" 1, "calt" 1, "pnum" 1, "tnum" 0,
+      "onum" 1, "lnum" 0, "dlig" 0, "subs" 1;
+  }
+  input[type="color"],
+  input[type="date"],
+  input[type="datetime"],
+  input[type="datetime-local"],
+  input[type="number"],
+  input[type="range"],
+  input[type="tel"],
+  input[type="week"] {
+    -webkit-font-feature-settings: "kern" 0, "liga" 1, "calt" 1, "pnum" 1,
+      "tnum" 0, "onum" 0, "lnum" 1, "zero" 0;
+    -moz-font-feature-settings: "kern" 0, "liga" 1, "calt" 1, "pnum" 1, "tnum" 0,
+      "onum" 0, "lnum" 1, "zero" 0;
+    font-feature-settings: "kern" 0, "liga" 1, "calt" 1, "pnum" 1, "tnum" 0,
+      "onum" 0, "lnum" 1, "zero" 0;
+  }
+  tbody,
+  caption {
+    -webkit-font-feature-settings: "kern" 1, "liga" 1, "calt" 1, "pnum" 0,
+      "tnum" 1, "onum" 0, "lnum" 1, "zero" 1;
+    -moz-font-feature-settings: "kern" 1, "liga" 1, "calt" 1, "pnum" 0, "tnum" 1,
+      "onum" 0, "lnum" 1, "zero" 1;
+    font-feature-settings: "kern" 1, "liga" 1, "calt" 1, "pnum" 0, "tnum" 1,
+      "onum" 0, "lnum" 1, "zero" 1;
+  }
+`;
+const bootstrap = {
+  space: [0, "0.25rem", "0.5rem", "1rem", "1.5rem", "3rem"],
+  fontSizes: ["1rem","1.25rem","1.5rem","1.75rem","2rem","2.5rem","3rem","3.5rem","4rem","4.5rem","5rem","5.5rem","6rem"],
+};
+export const Normalize = createGlobalStyle`${normalize}`;
+export const NormalizeOpenType = createGlobalStyle`${normalizeOpenType}`;
+export { ThemeProvider, bootstrap };
 export default Div;
