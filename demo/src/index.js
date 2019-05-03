f02993cab8237a0829f6c981e89aae385e991ada
Imed Adel <aadelimed@gmail.com>
2019-05-03 15:35:48 +0100

    Remove bootstrap theme from exports

--- demo/src/index.js
+++ demo/src/index.js
@@ -4,9 +4,41 @@ import {ThemeProvider} from "styled-components"
 
 import Div, {
   Normalize,
-  NormalizeOpenType,
-  bootstrap
+  NormalizeOpenType
 } from "../../src";
+
+const bootstrap = {
+  space: [0, "0.25rem", "0.5rem", "1rem", "1.5rem", "3rem"],
+  fontSizes: [
+    "1rem",
+    "1.25rem",
+    "1.5rem",
+    "1.75rem",
+    "2rem",
+    "2.5rem",
+    "3rem",
+    "3.5rem",
+    "4rem",
+    "4.5rem",
+    "5rem",
+    "5.5rem",
+    "6rem"
+  ],
+  hover: {
+    dim: {
+      opacity: "1",
+      transition: "opacity .15s ease-in",
+      "&:hover,&:focus": {
+        opacity: "0.5",
+        transition: "opacity .15s ease-in"
+      },
+      "&:active": {
+        opacity: "0.8",
+        transition: "opacity .15s ease-in"
+      }
+    }
+  }
+};
 
 const Demo = () => (
   <ThemeProvider theme={bootstrap}>
